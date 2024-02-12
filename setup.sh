feef16f8153227a93b261bfc08a0bb50f2b7945c
tripleseven190504 <tripleseven190504@gmail.com>
2024-02-12 09:35:45 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 sudo apt-get -y update
-sudo apt-get -y install python3 python3-pip
-pip install --break-system-packages --user -r requirements.txt
-wget -N https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chrome-linux64.zip -P ~/
+sudo apt-get -y install python3 python3-pip unzip
+pip install -r requirements.txt
+
+# Download Chrome and Chromedriver
+wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
 wget -N https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip -P ~/
-unzip ~/chrome-linux64.zip -d ~/chrome-linux64
-unzip ~/chromedriver-linux64.zip -d ~/chromedriver-linux64
-rm ~/chrome-linux64.zip
-rm ~/chromedriver-linux64.zip
+
+# Install Chrome and Chromedriver
+sudo dpkg -i ~/google-chrome-stable_current_amd64.deb
+unzip ~/chromedriver_linux64.zip -d ~/
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo mv -f ~/chrome-linux64/chrome /usr/local/bin/chrome
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chrome
-sudo chmod 0755 /usr/local/bin/chrome
-sudo chmod 0755 /usr/local/bin/chromedriver
+
+# Cleanup
+rm ~/google-chrome-stable_current_amd64.deb
+rm ~/chromedriver_linux64.zip
