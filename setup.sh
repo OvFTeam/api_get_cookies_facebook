c9e445e260ad55dac24183d6e32d51c58e0b3e19
tripleseven190504 <tripleseven190504@gmail.com>
2024-02-12 09:45:10 +0700

    push file

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 sudo apt-get -y update
 sudo apt-get -y install python3 python3-pip unzip
+mkdir -p ~/.config/pip/
+echo "[global]" >> ~/.config/pip/pip.conf
+echo "break-system-packages = true" >> ~/.config/pip/pip.conf
 pip install -r requirements.txt
 
-# Download Chrome and Chromedriver
 wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P ~/
 wget -N https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/linux64/chromedriver-linux64.zip -P ~/
 
-# Install Chrome and Chromedriver
 sudo dpkg -i ~/google-chrome-stable_current_amd64.deb
 unzip ~/chromedriver_linux64.zip -d ~/
 sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+sudo chmod 0755 /usr/local/bin/chromedriver
 
-# Cleanup
 rm ~/google-chrome-stable_current_amd64.deb
 rm ~/chromedriver_linux64.zip
